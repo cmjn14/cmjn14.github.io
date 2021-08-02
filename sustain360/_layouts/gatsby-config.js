ef726853309d83154cb76bf61f230eec0c324ab2
cmjn14 <cedric.lopez@free.fr>
2021-08-02 18:20:36 +0200

    unzip

---
+++ sustain360/_layouts/gatsby-config.js
@@ -0,0 +1,120 @@
+module.exports = {
+  siteMetadata: {
+    title: `Foam`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-garden`,
+      options: {
+        rootNote: "/readme",
+        contentPath: `${__dirname}/..`,
+        ignore: [
+          "**/_layouts/**",
+          "**/.git/**",
+          "**/.github/**",
+          "**/.vscode/**",
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        name: "paths",
+        engine: "flexsearch",
+        query: `{
+          allFile {
+            nodes {
+              id
+              ext
+              fields {
+                title
+                slug
+              }
+              childMdx {
+                excerpt
+              }
+            }
+          }
+        }`,
+
+        index: ["path"],
+
+        store: ["id", "path", "title", "excerpt"],
+
+        normalizer: ({ data }) =>
+          data.allFile.nodes
+            .filter((node) => node.ext === ".md")
+            .map((node) => ({
+              id: node.id,
+              path: node.fields.slug,
+              title: node.fields.title,
+              // Replace weirdly formatted [ link ] in excerpt to look like wikilinks ([link])
+              excerpt: node.childMdx.excerpt.replace(
+                /\[\s([\w'-]+)\s\]/gi,
+                (_, p1) => `[${p1}]`
+              ),
+            })),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        name: "titles",
+        engine: "flexsearch",
+        query: `{
+          allFile {
+            nodes {
+              id
+              ext
+              fields {
+                title
+              }
+            }
+          }
+        }`,
+
+        index: ["title"],
+
+        store: [],
+
+        normalizer: ({ data }) =>
+          data.allFile.nodes
+            .filter((node) => node.ext === ".md")
+            .map((node) => ({
+              id: node.id,
+              title: node.fields.title,
+            })),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        name: "bodies",
+        engine: "flexsearch",
+        query: `{
+          allFile {
+            nodes {
+              id
+              ext
+              childMdx {
+                rawBody
+              }
+            }
+          }
+        }`,
+
+        index: ["body"],
+
+        store: [],
+
+        normalizer: ({ data }) =>
+          data.allFile.nodes
+            .filter((node) => node.ext === ".md")
+            .map((node) => ({
+              id: node.id,
+              body: node.childMdx.rawBody,
+            })),
+      },
+    },
+  ],
+};
